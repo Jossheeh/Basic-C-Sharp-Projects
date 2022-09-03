2b6fc3ec363e4a0bff8c79788a2e598293a6d859
Jossheeh <83291710+Jossheeh@users.noreply.github.com>
2022-09-03 11:03:08 +0100

    Daily Report

---
+++ Daily Report/Daily Report/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Pitman Training");
+        Console.WriteLine("Student Daily Report");
+        Console.WriteLine("What course are you in?");
+        string s1 = Console.ReadLine();
+        Console.WriteLine("You are in {0}, What page number?",s1);
+        string s2 = Console.ReadLine();
+        Console.WriteLine("You are on page {0}, Do you need help with anything? Please answer \"true or \"false ", s2);
+        string s3 = Console.ReadLine();
+        Console.WriteLine("You answered {0}, Were there any positive experiences you’d like to share?  Please provide specifics", s3);
+        string s4 = Console.ReadLine();
+        Console.WriteLine("Is there any other feedback you’d like to provide?  Please be specific.", s4);
+        string s5 = Console.ReadLine();
+        Console.WriteLine("How many hours did you study today?", s5);
+        string s6 = Console.ReadLine();
+        Console.WriteLine("Thank you for your responses.  An Instructor will respond shortly.  Have a great day!” This will be the end of the program.", s6);
+        string s7 = Console.ReadLine();
+
+
+    }
+}
+
