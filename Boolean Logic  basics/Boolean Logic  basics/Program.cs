2364ebe28a900b8539e89b9fcf4567988e843269
Jossheeh <83291710+Jossheeh@users.noreply.github.com>
2022-09-11 11:27:17 +0100

    Car Insurance Approval Program

---
+++ Boolean Logic  basics/Boolean Logic  basics/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Boolean_Logic__basics
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //And operator
+            Console.WriteLine(true && false);
+            Console.WriteLine(true && true);
+            Console.WriteLine(false && false);
+
+            //Or operator
+            Console.WriteLine(true || true);
+            Console.WriteLine(true || false);
+            Console.WriteLine(false || false);
+
+            //equals operator
+            Console.WriteLine(true == true);
+            Console.WriteLine(true == false);
+            Console.WriteLine(false == false);
+
+            //not equal operator
+            Console.WriteLine(true != true);
+            Console.WriteLine(true != false);
+            Console.WriteLine(false != false);
+
+            //x or operator(one is true but not both)
+            Console.WriteLine(true ^ true);
+            Console.WriteLine(true ^ false);
+            Console.WriteLine(false ^ false);
+
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Car Insurance Approval/Car Insurance Approval/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Car_Insurance_Approval
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int Age;
+            string Dui;
+            int Tickets;
+
+
+            Console.WriteLine("Lets see if you qualify for car insurance!");
+            Console.WriteLine("What is your age?");
+            Age = Convert.ToInt32(Console.ReadLine());
+
+
+            Console.WriteLine("Have you ever had a DUI Violation? [i.e., for driving under the influence of alcohol]");
+            Dui = Console.ReadLine();
+            bool trueorfalse = (Dui == "yes" || Dui == "yes" || Dui == "YES");
+            Console.WriteLine(trueorfalse);
+
+
+            Console.WriteLine("How many speeding tickets have you had? ");
+            Tickets = Convert.ToInt32(Console.ReadLine());
+
+
+            Console.WriteLine("Qualified? ");
+            bool qualify = (Age > 15 && (Dui == "No" || Dui == "no" || Dui == "NO") && Tickets < 3);
+            Console.WriteLine(qualify);
+            Console.ReadLine();
+        }
+    }
+}
