3ac2a93ac8863fa266a88a7114fefe18822a073c
Jossheeh <83291710+Jossheeh@users.noreply.github.com>
2022-11-24 08:55:28 +0000

    String assignment

---
+++ String Assignment/String Assignment/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace String_Assignment
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+               string name = "Name: Josh";
+                  String age = " Age: 29";
+                  String area = " Area: Cork";
+
+            name = name.ToUpper();
+            Console.WriteLine(name + age + area);
+               
+          
+
+
+            StringBuilder sb = new StringBuilder();
+
+            sb.Append("My name is Josh \n I am 29 Years old \n I live in Cork.");
+
+            Console.WriteLine(sb);
+            Console.ReadLine();
+        }
+    }
+}
