30ef7cc26eec596ea23eaf43000a86672f0d8c7f
Jossheeh <83291710+Jossheeh@users.noreply.github.com>
2023-01-10 08:45:36 +0000

    Method Assignment

---
+++ MethodAssignment/MethodAssignment/Number.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodAssignment
+{
+    public class Number
+    {
+        int userInput;
+        public Number()
+        
+        {
+            Console.WriteLine("Please enter a number");
+            userInput = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("You picked {0} ", userInput);
+            Console.ReadLine();
+            Console.WriteLine("Your number multiplied by 50 is: " + userInput * 50);
+            Console.ReadLine();
+            int total = userInput + 50;
+            Console.WriteLine("Your number + 50 is: " + total.ToString());
+            Console.ReadLine();
+            Console.WriteLine("Your number divided by 2 is: " + userInput / 2);
+            Console.ReadLine();
+        }
+
+        }
+        
+    }
+
+
+
+
