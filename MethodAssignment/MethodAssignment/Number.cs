715b6c926d3535949fc4047195146ba0ef7c28b2
Jossheeh <83291710+Jossheeh@users.noreply.github.com>
2023-02-02 18:15:10 +0000

    Method_Assignment

--- MethodAssignment/MethodAssignment/Number.cs
+++ MethodAssignment/MethodAssignment/Number.cs
@@ -4,31 +4,33 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace MethodAssignment
+namespace Classes_and_Methods_Assignment
 {
     public class Number
     {
-        int userInput;
-        public Number()
-        
+        public int Math1(int num1, int num2)
         {
-            Console.WriteLine("Please enter a number");
-            userInput = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("You picked {0} ", userInput);
-            Console.ReadLine();
-            Console.WriteLine("Press return to continue");
-            Console.WriteLine("Your number multiplied by 50 is: " + userInput * 50);
-            Console.ReadLine();
-            int total = userInput + 50;
-            Console.WriteLine("Your number + 50 is: " + total.ToString());
-            Console.ReadLine();
-            Console.WriteLine("Your number divided by 2 is: " + userInput / 2);
-            Console.ReadLine();
-        }
 
+            int result;
+            result = num1 + num2;
+            return result;
         }
-        
+        public int Math2(int num1, int num2)
+        {
+
+            int result;
+            result = num1 - num2;
+            return result;
+        }
+        public int Math3(int num1, int num2)
+        {
+
+            int result;
+            result = num1 * num2;
+            return result;
+        }
     }
+}
 
 
 
--- MethodAssignment/MethodAssignment/Program.cs
+++ MethodAssignment/MethodAssignment/Program.cs
@@ -4,20 +4,46 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace MethodAssignment
+namespace Classes_and_Methods_Assignment
 {
-    internal class Program
+    public class Program
     {
         static void Main(string[] args)
         {
-            Number Number = new Number();
-            {
+            Console.WriteLine("Please enter a number: ");
+            int a = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Please enter a second number");
+            int b = Convert.ToInt32(Console.ReadLine());
+            int ret;
+
+            Number n = new Number();
 
 
-            }
+            ret = n.Math1(a, b);
+            Console.WriteLine("Your numbers added are : {0}", ret);
+
+
+            Number n1 = new Number();
+
+
+            ret = n1.Math2(a, b);
+            Console.WriteLine("Your numbers subtracted are : {0}", ret);
+
+            Number n2 = new Number();
+
+
+            ret = n2.Math3(a, b);
+            Console.WriteLine("Your numbers multiplied are : {0}", ret);
+
+            Console.ReadLine();
+
+
+
         }
-    }
-}
-    
 
-            
+    }
+
+}
+
+
+
