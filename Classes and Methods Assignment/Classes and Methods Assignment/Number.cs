3a69c4f7df8a18d90f7686940f74aa24a9c2c456
Jossheeh <83291710+Jossheeh@users.noreply.github.com>
2023-02-06 10:18:35 +0000

    Classes and Methods Assignment

---
+++ Classes and Methods Assignment/Classes and Methods Assignment/Number.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classes_and_Methods_Assignment
+{
+    public class Number
+    {
+        public int Math1(int num1, int num2)
+        {
+            
+            int result;
+                result = num1 + num2 ;
+            return result;
+        }
+        public double Math1( double num1, double num2)
+        {
+
+            double result;
+            result =   num1 + num2;
+            return result;
+        }
+        public int Math1(int num1, int num2, int f)
+        {
+
+            int result;
+            result = num1 + num2 - f ;
+            return result;
+        }
+
+    }
+}
+
+    
+
---
+++ Classes and Methods Assignment/Classes and Methods Assignment/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classes_and_Methods_Assignment
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter a number: ");
+            int a = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Please enter a second number");
+            int b = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Please enter a decimal number: ");
+            double c = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Please enter a second decimal number");
+            double d = Convert.ToDouble(Console.ReadLine());
+            int ret;
+            double reta;
+            int retb;
+            Console.WriteLine("Please enter one more number");
+            string e = Console.ReadLine();
+            int f;
+            Int32.TryParse(e, out f);
+
+
+
+            Number n = new Number();
+
+        
+            ret = n.Math1(a, b);
+            Console.WriteLine("Your numbers added are : {0}", ret);
+
+
+            reta = n.Math1(c, d);
+            Console.WriteLine("Your decimal numbers added are : {0}", reta);
+
+            retb = n.Math1(a, b, f);
+            Console.WriteLine("Your first two numbers added minus your last number is : {0}", retb);
+
+
+
+            Console.ReadLine();
+
+
+
+        }
+        
+    }
+
+}
+
