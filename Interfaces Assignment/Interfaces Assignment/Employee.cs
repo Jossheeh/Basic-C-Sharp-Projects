61a9d5a32625d968d340352c5cb4129c965bf813
Jossheeh <83291710+Jossheeh@users.noreply.github.com>
2023-02-06 18:04:01 +0000

    Interfaces Assignment

---
+++ Interfaces Assignment/Interfaces Assignment/Employee.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interfaces_Assignment
+{
+
+    public class Employee : Person, IQuittable
+    {
+        public void SayName()
+        {
+            throw new NotImplementedException();
+        }
+        public void Quit(Person person)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
+
---
+++ Interfaces Assignment/Interfaces Assignment/Person.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace Interfaces_Assignment
+{
+    public abstract class Person
+    {
+        public List<string> FirstName { get; set; }
+        public List<string> LastName { get; set; }
+        public List<string> FullName { get; set; }
+
+
+        public void ListSayName()
+        {
+
+
+            foreach (string name3 in FullName)
+            {
+                Console.WriteLine(name3);
+            }
+
+
+
+
+        }
+    }
+}
