eaf2c67d475ac8964ff2805d8110c1a71da001a0
Jossheeh <83291710+Jossheeh@users.noreply.github.com>
2023-02-06 12:53:56 +0000

    Classes and Methods Assignment 3

---
+++ ClassesAndMethods3/ClassesAndMethods3/Number.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classes_and_Methods_Assignment
+{
+    public class Number
+    {
+
+        public void Math1(int a)
+        {
+          
+                Console.WriteLine("Your first number is: " + a);
+            }
+
+            public void maths(int b)
+            {
+                Console.WriteLine("Your second number multiplied by 2 is: " + b * 2);
+            }
+
+        }
+}
+
+
---
+++ ClassesAndMethods3/ClassesAndMethods3/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classes_and_Methods_Assignment
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter a number: ");
+            int a = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Please enter a second number");
+            int b = Convert.ToInt32(Console.ReadLine());
+        
+
+            Number n = new Number();
+            n.Math1(a);
+            n.maths(b);
+
+            Console.ReadLine();
+
+        }
+
+
+
+
+
+    }
+        
+    }
+
+
