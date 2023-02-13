1c3d12ea29914132199301aec32704aed126b067
Jossheeh <83291710+Jossheeh@users.noreply.github.com>
2023-02-13 11:56:52 +0000

    Operator Overload

---
+++ OperatorOverloadingAssignment/OperatorOverloadingAssignment/Employee.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OperatorOverloadingAssignment
+{
+    public class Employee
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+
+        public static bool operator ==(Employee emp1, Employee emp2)
+        {
+            return emp1.ID == emp2.ID; 
+        }
+
+        public static bool operator != (Employee emp1, Employee emp2)
+        {
+            return !(emp1 == emp2);
+        }
+    }
+}
---
+++ OperatorOverloadingAssignment/OperatorOverloadingAssignment/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OperatorOverloadingAssignment
+{
+     class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee emp1 = new Employee { ID = 101, Name = "Josh" };
+            Employee emp2 = new Employee { ID = 102, Name = "Bill" };
+            Employee emp3 = new Employee { ID = 101, Name = "Amy" };
+
+            Console.WriteLine(emp1 == emp2);
+            Console.WriteLine(emp1 == emp3);
+
+            Console.ReadLine();
+        }
+    }
+}
