80d894c90cd8940e5eb626a1882d760ddf714684
Jossheeh <83291710+Jossheeh@users.noreply.github.com>
2023-02-13 12:57:57 +0000

    Generics Assignment

---
+++ Generics Assignment/Generics Assignment/Employee.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generics_Assignment
+{
+
+    class Employee<T>
+    {
+        public List<T> Things { get; set; }
+
+        public Employee(List<T> things)
+        {
+            Things = things;
+        }
+    }
+
+}
+
---
+++ Generics Assignment/Generics Assignment/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generics_Assignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee<string> employee1 = new Employee<string>(new List<string> { "apple", "banana", "cherry" });
+            Employee<int> employee2 = new Employee<int>(new List<int> { 1, 2, 3, 4, 5 });
+
+            Console.WriteLine("Employee 1 Things:");
+            foreach (string thing in employee1.Things)
+            {
+                Console.WriteLine(thing);
+            }
+
+            Console.WriteLine("Employee 2 Things:");
+            foreach (int thing in employee2.Things)
+            {
+                Console.WriteLine(thing);
+            }
+            Console.ReadLine();
+        }
+    }
+}
