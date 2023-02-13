57e11db67bd75019b2f670faf55383c77f1c342e
Jossheeh <83291710+Jossheeh@users.noreply.github.com>
2023-02-13 10:52:47 +0000

    ClassMethod Assignment

---
+++ Class Method Assignment/Class Method Assignment/Number.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Class_Method_Assignment
+{
+    public class Number
+    {
+        public void Math1(int a)
+        {
+            int result = (a / 2);
+            Console.WriteLine("The first number divided by 2 is: " + result);
+        }
+
+        public static void Math1(int a, int b, out int result)
+        {
+          
+            result = a + b;
+        }
+
+
+
+
+
+    }
+
+
+
+}
---
+++ Class Method Assignment/Class Method Assignment/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Class_Method_Assignment
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Please enter a number: ");
+            int a = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Please enter a second number");
+            int b = Convert.ToInt32(Console.ReadLine());
+            int result;
+
+            Number n = new Number();
+            n.Math1(a);
+
+            Number.Math1(a, b, out result);
+            Console.WriteLine("Your numbers added are: " + result);
+
+
+            Console.ReadLine();
+        }
+    }
+}
