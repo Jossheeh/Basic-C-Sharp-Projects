6f5486b75cf2a6a6983c16366b6aa37ef3410830
Jossheeh <83291710+Jossheeh@users.noreply.github.com>
2023-02-13 14:53:02 +0000

    Structs Assignment

---
+++ StructsAssignment/StructsAssignment/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StructsAssignment
+{
+    struct Number
+    {
+        public decimal Amount { get; set; }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Number number = new Number();
+            number.Amount = 12.34m;
+            Console.WriteLine(number.Amount);
+            Console.ReadLine();
+        }
+    }
+}
