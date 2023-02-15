0ecbb8811118ff89e412ce03942919832f68d302
Jossheeh <83291710+Jossheeh@users.noreply.github.com>
2023-02-15 10:24:52 +0000

    Date time Assignment

---
+++ DateTime Assignment/DateTime Assignment/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DateTime_Assignment
+{
+
+    class Program
+    {
+        static void Main()
+        {
+
+            Console.WriteLine("Current date and time: " + DateTime.Now);
+
+            Console.Write("Enter a number of hours: ");
+            int hours = Convert.ToInt32(Console.ReadLine());
+
+            DateTime futureTime = DateTime.Now.AddHours(hours);
+            Console.WriteLine("Time after " + hours + " hours: " + futureTime);
+            Console.ReadLine();
+        }
+    }
+}
