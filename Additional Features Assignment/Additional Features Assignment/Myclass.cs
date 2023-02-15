d79f8b36f5dc4882c5baa96380a1ff76ccadd526
Jossheeh <83291710+Jossheeh@users.noreply.github.com>
2023-02-15 15:12:55 +0000

    Additional Features Assignment

---
+++ Additional Features Assignment/Additional Features Assignment/Myclass.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Additional_Features_Assignment
+{
+        class MyClass
+        {
+            public string MyString { get; }
+
+            public MyClass() : this("default")
+            {
+            }
+
+            public MyClass(string myString)
+            {
+                MyString = myString;
+            }
+        }
+
+    }
+
---
+++ Additional Features Assignment/Additional Features Assignment/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Additional_Features_Assignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+                const int myConstVar = 42; 
+                var myVar = "Hello, everyone"; 
+
+                var myObj = new MyClass("ABC"); 
+
+                Console.WriteLine(myConstVar);
+                Console.WriteLine(myVar);
+                Console.WriteLine(myObj.MyString);
+
+            Console.ReadLine();
+            }
+        }
+
+        
+    }
+
