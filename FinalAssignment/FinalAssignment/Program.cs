a9477b703cfded186e08fea302aca6504ce5be77
Jossheeh <83291710+Jossheeh@users.noreply.github.com>
2023-02-20 10:21:05 +0000

    Final Assignment

---
+++ FinalAssignment/FinalAssignment/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FinalAssignment;
+
+ class Program
+{
+    public static void Main(string[] args)
+    {
+        using (StudentDbContext db = new StudentDbContext())
+        {
+            var student = new Student { Name = "John Smith", Age = 20 };
+            db.Students.Add(student);
+            db.SaveChanges();
+        }
+
+    }
+}
